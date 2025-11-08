1563abe5d7b0b47f7f86736b6731b669a49d94a0
zeusindomitable-max <zeusindomitable@gmail.com>
2025-11-09 01:42:19 +0700

    Create __init__.py

---
+++ ebc_suite/__init__.py
@@ -0,0 +1,7 @@
+from .ebc import energy_budget_clip
+from .act_guardian import act_guardian
+from .lr_auto import lr_auto
+from .mem_safe import mem_safe_forward
+
+__version__ = "0.1.0"
+__all__ = ["energy_budget_clip", "act_guardian", "lr_auto", "mem_safe_forward"]
